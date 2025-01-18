eae44dd22991032462f2578a5d2fe946868284ad
taeheee02 <fullsun2002@naver.com>
2025-01-18 22:05:07 +0900

    init

---
+++ src/main/java/PlayList/Matcher/MatcherApplication.java
@@ -0,0 +1,13 @@
+package PlayList.Matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PlayList/Matcher/MatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package PlayList.Matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
