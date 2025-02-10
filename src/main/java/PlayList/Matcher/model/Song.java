0fed21667658ff68a69f50dc0993ac176464a043
taeheee02 <fullsun2002@naver.com>
2025-02-10 21:59:37 +0900

    연습

--- src/main/java/PlayList/Matcher/model/Song.java
+++ src/main/java/PlayList/Matcher/model/Song.java
@@ -2,10 +2,8 @@ package PlayList.Matcher.model;
 
 public class Song {
 
-    private long id;
     private String title;
     private String artist;
-    private String appleMusicId;
     private MatchStatus matchStatus;
 
 }
