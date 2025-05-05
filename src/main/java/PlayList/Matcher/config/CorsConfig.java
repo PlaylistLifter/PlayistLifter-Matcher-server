c4326150b16918c1da50e70dfe005a50504cbc8e
TAEJUN <tjryu0302@naver.com>
2025-05-05 14:50:36 +0900

    프론트 변경

--- src/main/java/PlayList/Matcher/config/CorsConfig.java
+++ src/main/java/PlayList/Matcher/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); // 쿠키 및 인증정보 허용
-        config.setAllowedOrigins(List.of("http://localhost:8080")); // 허용할 도메인
+        config.setAllowedOrigins(List.of("http://localhost:8080","http://localhost:3000")); // 허용할 도메인
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
         config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
 
